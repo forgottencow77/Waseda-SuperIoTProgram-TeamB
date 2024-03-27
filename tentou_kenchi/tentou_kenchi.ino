ae3f20f552c40f99389343c52edab638f730198c
TENKAU <forgottencow77@gmail.com>
2024-03-27 15:07:15 +0900

    aieuo

--- tentou_kenchi/tentou_kenchi.ino
+++ tentou_kenchi/tentou_kenchi.ino
@@ -9,13 +9,13 @@ void setup() {
 
 
 //圧力計測変数
-  float A = 880.79;
-  float B = 47.96;
-  int aIn; // アナログ入力 A/D値
-  float aInV; // アナログ入力電圧[V]
-  int RM_kohm = 10.0; // 既知抵抗[kΩ]
-  float fsrR_kohm; // FSR抵抗値[kΩ]
-  float gram; // 推定質量 [gram]
+float A = 880.79;
+float B = 47.96;
+int aIn0, aIn1; // アナログ入力 A/D値 for A0 and A1
+float aInV0, aInV1; // アナログ入力電圧[V] for A0 and A1
+int RM_kohm = 10.0; // 既知抵抗[kΩ]
+float fsrR_kohm0, fsrR_kohm1; // FSR抵抗値[kΩ] for A0 and A1
+float gram0, gram1; // 推定質量 [gram] for A0 and A1
 
 //温度計測変数
   int tempIn;
@@ -27,21 +27,42 @@ void loop() {
   tempIn = analogRead(2);
   float tempC = tempIn * (5.0 / 1023.0) * 100.0;
 
-  // 質量の計算
-  aIn = analogRead(0); // アナログ入力(A/D変換値)
-  aInV = aIn * 5.0 / 1023; // A/D変換値から FSRにかかる電圧[V]を計算
-  fsrR_kohm = RM_kohm * aInV / (5.0 - aInV); // FSRの抵抗値[kΩ]
-  gram = A / fsrR_kohm + B; // 質量を計算[gram](実験値)
- 
-  // 質量&温度観測log 
-  Serial.print(aInV); // 力センサにかかる電圧の表示
+  // 質量の計算 for A0
+  aIn0 = analogRead(0); // アナログ入力(A/D変換値)
+  aInV0 = aIn0 * 5.0 / 1023; // A/D変換値から FSRにかかる電圧[V]を計算
+  fsrR_kohm0 = RM_kohm * aInV0 / (5.0 - aInV0); // FSRの抵抗値[kΩ]
+  gram0 = A / fsrR_kohm0 + B; // 質量を計算gram
+
+  // 質量の計算 for A1
+  aIn1 = analogRead(1); // アナログ入力(A/D変換値)
+  aInV1 = aIn1 * 5.0 / 1023; // A/D変換値から FSRにかかる電圧[V]を計算
+  fsrR_kohm1 = RM_kohm * aInV1 / (5.0 - aInV1); // FSRの抵抗値[kΩ]
+  gram1 = A / fsrR_kohm1 + B; // 質量を計算gram
+
+  // 質量①print
+  Serial.print("Sensor A0: ");
+  Serial.print(aInV0); // 力センサにかかる電圧の表示
   Serial.print("V"); // 単位の表示
   Serial.print("\t"); // タブ区切り
-  Serial.print(fsrR_kohm); // FSR抵抗値の表示
+  Serial.print(fsrR_kohm0); // FSR抵抗値の表示
   Serial.print(" kohm"); // 単位の表示
   Serial.print("\t"); // タブ区切り
-  Serial.print(gram); // 質量の表示
+  Serial.print(gram0); // 質量の表示
   Serial.println(" g"); // 単位の表示と改行
+
+  //質量②print
+  Serial.print("Sensor A1: ");
+  Serial.print(aInV1); // 力センサにかかる電圧の表示
+  Serial.print("V"); // 単位の表示
+  Serial.print("\t"); // タブ区切り
+  Serial.print(fsrR_kohm1); // FSR抵抗値の表示
+  Serial.print(" kohm"); // 単位の表示
+  Serial.print("\t"); // タブ区切り
+  Serial.print(gram1); // 質量の表示
+  Serial.println(" g"); // 単位の表示と改行
+
+  //温度print
+  Serial.println(tempIn);
   Serial.println(tempC); //温度
 
   if(ここに続き
